a3843aa85eb3e1da010073ef88685e00dc68a5cd
Umesh Desai <umesh.desai@mindstix.com>
2023-01-10 10:48:18 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+        int a= 5;
+        int b= 50;
+        System.out.println(a+b);
+
+    }
+}
