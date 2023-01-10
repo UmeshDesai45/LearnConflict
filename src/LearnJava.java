dfa1ca5276770f2f5ba59b7c12dcf54aa515c0d4
Umesh Desai <umesh.desai@mindstix.com>
2023-01-10 11:14:07 +0530

    Created new class for a,b

--- src/LearnJava.java
+++ src/LearnJava.java
@@ -1,3 +1,9 @@
 public class LearnJava {
+    int a;
+    int b;
+    LearnJava(int a, int b){
+        this.a= a;
+        this.b= b;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,8 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
-        int a= 5;
-        int b= 50;
-        System.out.println(a+b);
+        LearnJava l= new LearnJava(7,9);
+        System.out.println(l.a + l.b);
 
     }
 }
