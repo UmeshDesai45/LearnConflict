b44cf7f13aa9fcf10aef32f613937a0d270cc450
Umesh Desai <umesh.desai@mindstix.com>
2023-01-10 13:53:17 +0530

    Revert "removed first line"
    This reverts commit d4c05df75284e25ce73db4b0f5af810a39894fd5.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
 
+
+        int a= 25;
+        int b= 50;
+        System.out.println(a+b);
         LearnJava l= new LearnJava(7,9);
         System.out.println(l.a + l.b);
 
